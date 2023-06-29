1df12a3657db940f9795ffb9e4fa4030e6df3948
Mark Melton <mark.melton@notlem.com>
2023-06-29 16:27:17 -0400

    Update cmake configuration

--- cmake/load_cmake_helpers.cmake
+++ cmake/load_cmake_helpers.cmake
@@ -1,19 +1,14 @@
 
-if(CPP_CORE_DEV AND NOT CPP_CORE_DIR)
-  set(CPP_CORE_DIR "${CMAKE_CURRENT_LIST_DIR}/..")
-endif()
-
-if(CPP_CORE_DIR AND EXISTS ${CPP_CORE_DIR}/cmake)
-  set(CPP_CORE_CMAKE_DIR ${CPP_CORE_DIR}/cmake)
-else()
+if(NOT CPP_CORE_CMAKE_DIR)
   include(FetchContent)
+  set(NAME cpp_core_cmake)
   FetchContent_Declare(
-    cmake
+    ${NAME}
     GIT_REPOSITORY https://github.com/cpp-core/cmake
     GIT_TAG main
     )
-  FetchContent_MakeAvailable(cmake)
-  set(CPP_CORE_CMAKE_DIR ${CMAKE_BINARY_DIR}/_deps/cmake-src)
+  FetchContent_MakeAvailable(${NAME})
+  set(CPP_CORE_CMAKE_DIR ${CMAKE_BINARY_DIR}/_deps/${NAME}-src)
 endif()
 
 include(${CPP_CORE_CMAKE_DIR}/utils/all.cmake)
